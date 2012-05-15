aae9f59f6359851c262ab1d7183c269aa581cb11
Stephen Osondu <stephendirect@yahoo.com>
2012-05-15 16:31:49 +0100

    add all file

---
+++ exe1.rb
@@ -0,0 +1,15 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "this is fun."
+puts 'Yap! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+# this part of extra credit, the first time I run it, it
+# will add another line
+puts "I want my script to print another line"
+
+# this is extra credit, the next time it is run, it will 
+# print just one of the lines
+puts "Make my scrript print only one of the lines"
+
